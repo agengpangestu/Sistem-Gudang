f8487b6eccf1e66056e0cb95b85606c032e57b92
Ageng Pangestu <agengp617@gmail.com>
2024-08-25 12:40:09 +0700

    added file

---
+++ src/utils/prisma.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client"
+
+export const prisma = new PrismaClient({
+  log: ["error", "info", "query", "warn"]
+})
+class PrismaUtils {
+  public prisma: PrismaClient
+
+  constructor() {
+    this.prisma = new PrismaClient({
+      log: ["error", "info", "query", "warn"]
+    })
+  }
+
+  public async connect() {
+    await this.prisma.$connect()
+  }
+
+  public async disconnect() {
+    await this.prisma.$disconnect()
+  }
+}
+
+export const prismaUtils = new PrismaUtils()
