0041d9762d86932d2c1130f8610ccd8013ac74ee
Ageng Pangestu <agengp617@gmail.com>
2024-08-25 13:09:48 +0700

    make class,method service regsiter and login

---
+++ src/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Prisma } from "@prisma/client"
+import { prismaUtils } from "../utils/prisma"
+import AuthRegisterType from "../types/auth.type"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
+import UserType from "../types/user.type"
+
+class AuthService {
+  public async Register(payload: Omit<AuthRegisterType, "id,name">): Promise<AuthRegisterType> {
+    try {
+      return await prismaUtils.prisma.user.create({ data: payload })
+    } catch (error: any) {
+      if (error instanceof PrismaClientKnownRequestError && error.code === "P2002") {
+        throw new Error("Email has been registered")
+      }
+      throw error
+    }
+  }
+
+  public async Login(email: string): Promise<any> {
+    try {
+      return await prismaUtils.prisma.user.findUnique({ where: { email: email } })
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+}
+
+export default new AuthService()
