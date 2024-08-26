561d497f1394be9b17d47ea5ede7f2baac7417d1
Ageng Pangestu <agengp617@gmail.com>
2024-08-26 12:29:55 +0700

    create check user token and role,to protected route

---
+++ src/middleware/auth.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express"
+
+export const RequiredAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const user = res.locals.user
+
+  if (!user || user.role !== "admin")
+    return res
+      .status(403)
+      .json({
+        success: false,
+        message: "This route is protected and you don't has authority to access",
+        statusCode: 403
+      })
+
+  return next()
+}
