114adf3cb4c99514bf0d57414607a016d6e5c9f2
Ageng Pangestu <agengp617@gmail.com>
2024-08-26 14:24:33 +0700

    add authentication check role user for user.route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"
 import userController from "../controller/user.controller"
+import { RequiredAdmin } from "../middleware/auth"
 
 export const UserRoute: Router = Router()
 
 UserRoute.get("/", userController.GetAll)
-UserRoute.get("/:id", userController.GetById)
-UserRoute.put("/:id/update-user", userController.Update)
-UserRoute.delete("/:id/delete-user", userController.Destroy)
+UserRoute.get("/:id", RequiredAdmin, userController.GetById)
+UserRoute.put("/:id/update-user", RequiredAdmin, userController.Update)
+UserRoute.delete("/:id/delete-user", RequiredAdmin, userController.Destroy)
