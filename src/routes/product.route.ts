1ecde012e555eacc3df5d569286784b0765599a9
Ageng Pangestu <agengp617@gmail.com>
2024-08-27 14:30:16 +0700

    add guard for route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -5,5 +5,7 @@ import { RequiredAdmin } from "../middleware/auth"
 export const ProductRoute: Router = Router()
 
 ProductRoute.get("/", productController.GetAll)
-ProductRoute.get("/:id", productController.GetById)
+ProductRoute.get("/:id", RequiredAdmin, productController.GetById)
 ProductRoute.post("/post-product", RequiredAdmin, productController.Store)
+ProductRoute.put("/:id/update-product", RequiredAdmin, productController.Update)
+ProductRoute.delete("/:id/delete-product", RequiredAdmin, productController.Destroy)
