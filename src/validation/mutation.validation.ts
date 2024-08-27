0e80b8850f8cd1d8c02819cfee23c7facd057fe1
Ageng Pangestu <agengp617@gmail.com>
2024-08-27 21:40:44 +0700

    create mutation validation post and update

---
+++ src/validation/mutation.validation.ts
@@ -0,0 +1,24 @@
+import Joi from "joi"
+import MutationType from "../types/mutation.type"
+
+export const MutationValidation = (payload: MutationType) => {
+  const schema = Joi.object({
+    mutation_id: Joi.string().required(),
+    user_id: Joi.number().required(),
+    product_id: Joi.number().required(),
+    quantity: Joi.number().required(),
+    mutation_type: Joi.string().required()
+  }).options({ abortEarly: false })
+
+  return schema.validate(payload)
+}
+
+export const MutationUpdateValidation = (payload: MutationType) => {
+  const schema = Joi.object({
+    product_id: Joi.number(),
+    quantity: Joi.number(),
+    mutation_type: Joi.string()
+  }).options({ abortEarly: false })
+
+  return schema.validate(payload)
+}
