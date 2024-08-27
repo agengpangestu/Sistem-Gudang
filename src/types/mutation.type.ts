743db80648ac6dd15909f6d6f9e51ad0da5baf31
Ageng Pangestu <agengp617@gmail.com>
2024-08-27 21:40:26 +0700

    create mutation interface

---
+++ src/types/mutation.type.ts
@@ -0,0 +1,12 @@
+import { mutation_type } from "@prisma/client"
+
+export default interface MutationType {
+  int: number
+  mutation_id: string
+  user_id: number
+  product_id: number
+  quantity: number
+  mutation_type: mutation_type
+  createdAt: Date
+  updatedAt: Date
+}
