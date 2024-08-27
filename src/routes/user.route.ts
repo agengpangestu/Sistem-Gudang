ee11e2ad7ccc7699dbcb7a383f869023f6098043
Ageng Pangestu <agengp617@gmail.com>
2024-08-27 15:36:58 +0700

    add check role and token at route user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,7 +4,7 @@ import { RequiredAdmin } from "../middleware/auth"
 
 export const UserRoute: Router = Router()
 
-UserRoute.get("/", userController.GetAll)
+UserRoute.get("/", RequiredAdmin, userController.GetAll)
 UserRoute.get("/:id", RequiredAdmin, userController.GetById)
 UserRoute.put("/:id/update-user", RequiredAdmin, userController.Update)
 UserRoute.delete("/:id/delete-user", RequiredAdmin, userController.Destroy)
