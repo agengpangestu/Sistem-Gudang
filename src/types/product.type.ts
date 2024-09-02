48e6072052460847f2ee1ce7be3ee1b2830011ee
Ageng Pangestu <agengp617@gmail.com>
2024-09-02 21:27:10 +0700

    added extends n sort

--- src/types/product.type.ts
+++ src/types/product.type.ts
@@ -1,8 +1,9 @@
 import { Decimal } from "@prisma/client/runtime/library"
 
-export default interface ProductType {
+export interface ProductType extends ProductPagination {
   id: number
   product_id: string
+  product_code: number
   product_name: string
   desc: string
   location: string
@@ -10,4 +11,11 @@ export default interface ProductType {
   user_id: number
   createdAt: Date
   updatedAt: Date
+  sort_by?: "product_name" | "price"
+  sort_order?: "asc" | "desc"
+}
+
+export interface ProductPagination {
+  page: number
+  limit: number
 }
