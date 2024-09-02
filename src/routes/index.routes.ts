15219014a6f7b654f6cce054a510e0253919b229
Ageng Pangestu <agengp617@gmail.com>
2024-09-02 15:21:46 +0700

    added new route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -4,13 +4,17 @@ import { HealthRoute } from "./health.route"
 import { AuthRoute } from "./auth.route"
 import { ProductRoute } from "./product.route"
 import { MutationRoute } from "./mutation.route"
+import { QuantityRoute } from "./quantity.route"
+import { FakerRoute } from "./faker.route"
 
 const _routes: Array<[string, Router]> = [
   ["/api/health", HealthRoute],
   ["/api/authentication", AuthRoute],
   ["/api/user", UserRoute],
   ["/api/product", ProductRoute],
-  ["/api/mutation", MutationRoute]
+  ["/api/mutation", MutationRoute],
+  ["/api/quantity", QuantityRoute],
+  ["/api/faker", FakerRoute]
 ]
 
 export const routes = (app: Application) => {
