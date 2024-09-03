60948a815a16d4d5ae38edbca43d119d06c94b96
Ageng Pangestu <agengp617@gmail.com>
2024-09-03 16:19:31 +0700

    extends,add sort by role,sort order and pagination interface

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,6 +1,6 @@
 import { user_role } from "@prisma/client"
 
-export default interface UserType {
+export default interface UserType extends UserPagination {
   id: number
   user_id: string
   name: string
@@ -9,4 +9,11 @@ export default interface UserType {
   role: user_role
   createdAt: Date
   updatedAt: Date
+  sort_by_role?: "regular" | "admin" | "super_admin"
+  sort_order?: "asc" | "desc"
+}
+
+export interface UserPagination {
+  page: number,
+  limit: number
 }
