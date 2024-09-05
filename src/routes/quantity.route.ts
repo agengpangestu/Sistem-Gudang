2fff768f12b8f9218a9c81683e8ddafcb1054673
Ageng Pangestu <agengp617@gmail.com>
2024-09-05 20:07:05 +0700

    add req admin when fetch this endpoint

--- src/routes/quantity.route.ts
+++ src/routes/quantity.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
 import quantityController from "../controller/quantity.controller"
+import { RequiredAdmin } from "../middleware/auth"
 
 export const QuantityRoute: Router = Router()
 
-QuantityRoute.get("/", quantityController.GetAll)
-QuantityRoute.get("/total-quantity", quantityController.GetQuantityOfProduct)
+QuantityRoute.get("/", RequiredAdmin, quantityController.GetAll)
+QuantityRoute.get("/total-quantity", RequiredAdmin, quantityController.GetQuantityOfProduct)
