75b35b2249b97407c0d9d16aa6521f613f594695
Ageng Pangestu <agengp617@gmail.com>
2024-09-09 20:53:29 +0700

    make global handle error func

---
+++ src/helpers/global.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express"
+import Unauthorized from "./unauthorized"
+import { logger } from "../utils/logger"
+import ErrorNotFound from "./not.found"
+import Joi from "joi"
+import JoiError from "./joi"
+
+export const GlobalError = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Unauthorized) {
+    return res.status(403).json({
+      status: false,
+      name: err.name,
+      message: err.message,
+      statusCode: 403
+    })
+  } else if (err instanceof ErrorNotFound) {
+    return res.status(404).json({
+      status: false,
+      name: err.name,
+      message: err.message,
+      statusCode: 404
+    })
+  } else if (err instanceof JoiError) {
+    return res.status(422).json({
+      status: false,
+      name: err.name,
+      message: err.message.replace(/\"/g, ""),
+      details: err.details,
+      statusCode: 422
+    })
+  } else {
+    return res.status(500).json({
+      status: false,
+      message: "Something error",
+      statusCode: 500
+    })
+  }
+
+}
