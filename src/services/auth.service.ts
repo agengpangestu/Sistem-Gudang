0fca6b947a72ea8f797c7fd4e4da6e7f001c2a12
Ageng Pangestu <agengp617@gmail.com>
2024-09-09 20:51:35 +0700

    added constructor and change code little

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,13 +1,19 @@
 import { Prisma } from "@prisma/client"
-import { prismaUtils } from "../utils/prisma"
 import AuthRegisterType from "../types/auth.type"
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
 import UserType from "../types/user.type"
+import PrismaUtils from "../utils/prisma"
 
 class AuthService {
+  private prisma: PrismaUtils
+
+  constructor() {
+    this.prisma = new PrismaUtils()
+  }
+  
   public async Register(payload: Omit<AuthRegisterType, "id">): Promise<AuthRegisterType | any> {
     try {
-      return await prismaUtils.prisma.user.create({ data: payload })
+      return await this.prisma.users.create({ data: payload })
     } catch (error: any) {
       if (error instanceof PrismaClientKnownRequestError && error.code === "P2002") {
         throw new Error("Email has been registered")
@@ -18,7 +24,7 @@ class AuthService {
 
   public async Login(email: string): Promise<any> {
     try {
-      return await prismaUtils.prisma.user.findUnique({ where: { email: email } })
+      return await this.prisma.users.findUnique({ where: { email: email } })
     } catch (error) {
       console.log(error)
       throw error
