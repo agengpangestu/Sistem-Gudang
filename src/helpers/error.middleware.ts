541e6349247edc26080828af76371e139ff68a13
Ageng Pangestu <agengp617@gmail.com>
2024-09-10 11:51:23 +0700

    again

---
+++ src/helpers/error.middleware.ts
@@ -0,0 +1,10 @@
+class BaseError extends Error {
+  statusCode: number
+  constructor(statusCode: number, message: string) {
+    super()
+    this.statusCode = statusCode
+    this.message = message
+  }
+}
+
+export default BaseError
