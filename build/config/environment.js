dee38b094d277c356b0e7918be3fbf7b4aa56496
Ageng Pangestu <agengp617@gmail.com>
2024-09-10 13:31:46 +0700

    delete

--- build/config/environment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const CONFIG = {
-    jwt_public: process.env.JWT_PUBLIC,
-    jwt_private: process.env.JWT_PRIVATE
-};
-exports.default = CONFIG;
-//# sourceMappingURL=environment.js.map
