26d457fff71237f47cef3f3e5b138bd6a694ae40
Ageng Pangestu <agengp617@gmail.com>
2024-09-12 11:40:02 +0700

    added role

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,8 +3,9 @@ import Unauthorized from "../helpers/unauthorized"
 
 export const RequiredAdmin = (req: Request, res: Response, next: NextFunction) => {
   const user = res.locals.user
+  
 
-  if (!user || user.role !== "admin")
+  if (!user || user.role !== "admin" && user.role !== "super_admin")
     throw next(new Unauthorized)
 
   return next()
