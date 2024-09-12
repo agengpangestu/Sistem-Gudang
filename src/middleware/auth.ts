df716e1e31cca9d7a94a898cb524bffa393f2264
Ageng Pangestu <agengp617@gmail.com>
2024-09-12 11:56:13 +0700

    delete console.log()

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,8 +3,6 @@ import Unauthorized from "../helpers/unauthorized"
 
 export const RequiredAdmin = (req: Request, res: Response, next: NextFunction) => {
   const user = res.locals.user
-  console.log(user);
-  
 
   if (!user || user.role !== "admin")
     throw next(new Unauthorized)
