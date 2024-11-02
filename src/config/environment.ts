6768acd4791c47864dbb37cb1f7238c49215426a
Ageng Pangestu <agengp617@gmail.com>
2024-11-02 16:30:41 +0700

    feat: added port from env

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
 
 const CONFIG = {
+  port: process.env.APP_PORT as string,
   jwt_public: process.env.JWT_PUBLIC as string,
   jwt_private: process.env.JWT_PRIVATE as string
 }
