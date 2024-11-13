f341032e2f6e0f6ae43825339a85c4a4427adf3f
Ageng Pangestu <agengp617@gmail.com>
2024-11-13 12:52:25 +0700

    update: added auth logout(ongoing)

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,7 +1,13 @@
-import { Router } from "express"
+import { NextFunction, Request, Response, Router } from "express"
 import authController from "../controller/auth.controller"
 
 export const AuthRoute: Router = Router()
 
 AuthRoute.post("/registration", authController.Register)
 AuthRoute.post("/login", authController.Login)
+AuthRoute.get("/logout", (req: Request, res: Response, next: NextFunction) => {
+  //   res.clearCookie("accessClient")
+  //   res.clearCookie("accessToken")
+  console.log(req.headers)
+  res.status(200).send("Cookie deleted")
+})
