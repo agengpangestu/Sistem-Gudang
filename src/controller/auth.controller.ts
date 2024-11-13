b8a93f417eabee487254d9acd802c5a0f9f34465
agengpangestu <kapalmabur002@gmail.com>
2024-11-13 15:22:01 +0700

    update: - added error auth and validation - delete cookie user - send user to resposne

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -7,6 +7,8 @@ import { decrypt, encrypt } from "../utils/bcrypt"
 import { signJwt } from "../utils/jwt"
 import { logger } from "../utils/logger"
 import { LoginValidation, RegisterValidation } from "../validation/auth.validation"
+import ErrorAuth from "../helpers/error.auth"
+import ErrorValidation from "../helpers/error.validation"
 
 class AuthController {
   public async Register(req: Request, res: Response, next: NextFunction) {
@@ -32,35 +34,30 @@ class AuthController {
   public async Login(req: Request, res: Response, next: NextFunction) {
     const { error, value } = LoginValidation(req.body)
 
-    if (error) {
-      logger.error(`ERR Validation: auth - login = ${error.message}`)
-      return res.status(422).send({
-        status: false,
-        statusCode: 422,
-        message: error.name,
-        error_validation: joiError(error.details)
-      })
-    }
     try {
+      if (error) {
+        logger.error(`ERR Validation: auth - login = ${error.message}`)
+        throw next(new ErrorValidation("Error Validation", joiError(error.details)))
+      }
+
       const user: any = await authService.Login(value.email)
       const password = await prisma.user.findUnique({ where: { email: value.email } })
 
       if (!user) {
-        return res.status(422).send({ status: false, statusCode: 422, message: "Email not registered" })
+        throw next(new ErrorAuth("Error login", "Email not registered"))
       }
 
       const matching_password = decrypt(value.password, password?.password as string)
       if (!matching_password) {
-        return res.status(422).send({ status: false, statusCode: 422, message: "Password not match" })
+        throw next(new ErrorAuth("Error login", "Password not match"))
       }
 
       const access_token = signJwt({ ...user }, { expiresIn: "1d" })
 
       // set token to header
       // set user to header too
-      res.cookie("accessToken", access_token).cookie("accessClient", user).send()
+      res.cookie("accessToken", access_token).send(user)
     } catch (error: any) {
-      logger.error(`ERR: auth - login = ${error}`)
       next(error)
     }
   }
