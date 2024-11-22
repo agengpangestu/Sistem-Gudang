8ab3fe05f8d65c558391c5c776d8be60a709befa
agengpangestu <kapalmabur002@gmail.com>
2024-11-22 21:38:51 +0700

    update: import and use middleware validate error joi

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -3,6 +3,8 @@ import { ProductController } from "../controller/product.controller"
 import { RequiredAdmin } from "../middleware/auth"
 import PrismaUtils from "../utils/prisma"
 import ProductService from "../services/product.service"
+import { ValidateError } from "../middleware"
+import { ProductValidation } from "../validation"
 
 const prisma = new PrismaUtils()
 const productController = new ProductController(new ProductService(prisma))
@@ -10,6 +12,6 @@ export const ProductRoute: Router = Router()
 
 ProductRoute.get("/", productController.GetAll)
 ProductRoute.get("/:id", RequiredAdmin, productController.GetById)
-ProductRoute.post("/post-product", RequiredAdmin, productController.Store)
+ProductRoute.post("/post-product", RequiredAdmin, ValidateError(ProductValidation), productController.Store)
 ProductRoute.put("/:id/update-product", RequiredAdmin, productController.Update)
 ProductRoute.delete("/:id/delete-product", RequiredAdmin, productController.Destroy)
