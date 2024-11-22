f110f0772fcb4b97c2167b5d80d367d3718317da
agengpangestu <kapalmabur002@gmail.com>
2024-11-22 21:38:12 +0700

    update naming export

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,4 +1,4 @@
 export * from "./auth.validation"
 export * from "./user.validation"
 export * from "./mutation.validation"
-export * from "./product..validation"
+export * from "./product.validation"
