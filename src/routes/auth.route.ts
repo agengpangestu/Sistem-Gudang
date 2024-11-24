d6c565d23b2138ec109c21f681e0782ecd805ac7
agengpangestu <kapalmabur002@gmail.com>
2024-11-24 14:16:20 +0700

    update: - added prisma and service - added validate error for joi to route regsiter and  login

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,13 @@
 import { Router } from "express"
 import { AuthController } from "../controller"
+import PrismaUtils from "../utils/prisma"
+import { AuthService } from "../services"
+import { ValidateError } from "../middleware"
+import { LoginValidation, RegisterValidation } from "../validation"
 
-const authController = new AuthController()
+const prisma = new PrismaUtils()
+const authController = new AuthController(new AuthService(prisma))
 export const AuthRoute: Router = Router()
 
-AuthRoute.post("/registration", authController.Register)
-AuthRoute.post("/login", authController.Login)
+AuthRoute.post("/registration", ValidateError(RegisterValidation), authController.Register)
+AuthRoute.post("/login", ValidateError(LoginValidation), authController.Login)
