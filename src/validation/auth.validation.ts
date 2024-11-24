992b6ff35ae2462f493c615bbe9e632259a6ede5
agengpangestu <kapalmabur002@gmail.com>
2024-11-24 14:21:29 +0700

    refactor: change to object

--- src/validation/auth.validation.ts
+++ src/validation/auth.validation.ts
@@ -3,31 +3,25 @@ import UserType from "../types/user.type"
 import { user_role } from "@prisma/client"
 import { z } from "zod"
 
-export const RegisterValidation = (payload: UserType) => {
-  const schema = Joi.object({
-    user_id: Joi.string().required(),
-    name: Joi.string().required(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-    role: Joi.string().default(user_role.regular)
-  }).options({ abortEarly: false })
+export const RegisterValidation = Joi.object({
+  user_id: Joi.string().required(),
+  name: Joi.string().required(),
+  email: Joi.string().required().email(),
+  password: Joi.string().required(),
+  role: Joi.string().default(user_role.regular)
+}).options({ abortEarly: false })
 
-  return schema.validate(payload)
-}
-
-export const LoginValidation = (payload: UserType) => {
-  const schema = Joi.object({
-    email: Joi.string().required().email(),
-    password: Joi.string().required()
-  }).options({ abortEarly: false })
-
-  return schema.validate(payload)
-}
+export const LoginValidation = Joi.object({
+  email: Joi.string().required().email(),
+  password: Joi.string().required()
+}).options({ abortEarly: false })
 
 // schema using zod
 export const loginSchema = z
   .object({
-    email: z.string({required_error: "Email is required"}).email({message: "Invalid email address"}),
-    password: z.string({required_error: "Password is Required"}).min(8, {message: "Password length should be more than 8"})
+    email: z.string({ required_error: "Email is required" }).email({ message: "Invalid email address" }),
+    password: z
+      .string({ required_error: "Password is Required" })
+      .min(8, { message: "Password length should be more than 8" })
   })
   .partial()
