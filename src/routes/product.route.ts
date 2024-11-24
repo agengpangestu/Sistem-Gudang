4100c337e0faf5e1f9252a8131249719f20fb344
agengpangestu <kapalmabur002@gmail.com>
2024-11-24 11:14:27 +0700

    update: - change params id to product_code - added validate error joi for update router

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -4,14 +4,14 @@ import { RequiredAdmin } from "../middleware/auth"
 import PrismaUtils from "../utils/prisma"
 import ProductService from "../services/product.service"
 import { ValidateError } from "../middleware"
-import { ProductValidation } from "../validation"
+import { ProductUpdateValidation, ProductValidation } from "../validation"
 
 const prisma = new PrismaUtils()
 const productController = new ProductController(new ProductService(prisma))
 export const ProductRoute: Router = Router()
 
 ProductRoute.get("/", productController.GetAll)
-ProductRoute.get("/:id", RequiredAdmin, productController.GetById)
+ProductRoute.get("/:product_code", RequiredAdmin, productController.GetById)
 ProductRoute.post("/post-product", RequiredAdmin, ValidateError(ProductValidation), productController.Store)
-ProductRoute.put("/:id/update-product", RequiredAdmin, productController.Update)
-ProductRoute.delete("/:id/delete-product", RequiredAdmin, productController.Destroy)
+ProductRoute.put("/:product_code/update-product", RequiredAdmin, ValidateError(ProductUpdateValidation), productController.Update)
+ProductRoute.delete("/:product_code/delete-product", RequiredAdmin, productController.Destroy)
