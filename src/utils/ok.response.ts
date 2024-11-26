340c937f8ddca05ffd27cc9ddfa5413f48a450de
agengpangestu <kapalmabur002@gmail.com>
2024-11-26 15:48:22 +0700

    update: change type data parameter data from null to object

--- src/utils/ok.response.ts
+++ src/utils/ok.response.ts
@@ -1,5 +1,5 @@
 import { Response } from "express"
-export const successResponse = (res: Response, statusCode: number, message: string, data?: null): void => {
+export const successResponse = (res: Response, statusCode: number, message: string, data?: object): void => {
   res.status(statusCode).json({
     success: true,
     statusCode: statusCode,
