d4c34270250d8267e3f0e86a96bad06a8a8fc77b
agengpangestu <kapalmabur002@gmail.com>
2024-11-26 14:50:00 +0700

    update: - added user pagination response type - added user - added user detail - added user query - added user update

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,4 +1,6 @@
 import { user_role } from "@prisma/client"
+import { PaginationType } from "./pagination.type"
+import { QueryType } from "./query.type"
 
 export default interface UserType extends UserPagination {
   id: number
@@ -14,6 +16,41 @@ export default interface UserType extends UserPagination {
 }
 
 export interface UserPagination {
-  page: number,
+  page: number
   limit: number
+}
+
+// update
+// use this
+export interface UserPaginationResponse {
+  total_data: number
+  total_pages: number
+  current_page: number
+  data: User[]
+}
+
+export interface User {
+  user_id: string
+  name: string
+  email: string
+  role: user_role
+  createdAt: Date
+}
+
+export interface UserDetail extends Partial<User> {
+  id: number
+  password: string
+  updatedAt: Date
+}
+
+export interface UserQuery extends QueryType, PaginationType {
+  name: string
+  role: user_role
+}
+
+export interface UserUpdate {
+  name: string
+  email: string
+  password: string
+  role: user_role
 }
