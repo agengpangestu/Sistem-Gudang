26a4db851baa1005346d9bf1a7ff18b8e5deb682
agengpangestu <kapalmabur002@gmail.com>
2024-11-26 13:49:14 +0700

    refactor: - added consturctor - refactor payload store product service (faker) - added interface to store product service (faker) - added error database at service store product and user (faker) - refactor store service (faker) - added auth register for store user (faker)

--- src/services/faker.service.ts
+++ src/services/faker.service.ts
@@ -1,42 +1,46 @@
-import { Decimal, PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
-import { ProductType } from "../types/product.type"
-import UserType from "../types/user.type"
-import AuthRegisterType from "../types/auth.type"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
+import { v7 as uuidV7 } from "uuid"
+import { Product, ProductStore } from "../types/product.type"
+import AuthRegisterType, { AuthRegister } from "../types/auth.type"
 import PrismaUtils from "../utils/prisma"
+import { faker } from "@faker-js/faker"
+import DatabaseErrorConstraint from "../helpers/database"
 
-class FakerService {
-  private prisma: PrismaUtils
-
-  constructor () {
-    this.prisma = new PrismaUtils()
-  }
-  public async storeProduct(payload: Omit<ProductType, "id">): Promise<ProductType | any> {
+export class FakerService {
+  constructor(private prisma: PrismaUtils) {}
+  public async storeProduct(payload: ProductStore): Promise<Product> {
     try {
       return await this.prisma.products.create({
         data: {
           ...payload,
-          price: new Decimal(payload.price),
+          product_id: uuidV7(),
+          product_code: parseInt(faker.finance.accountNumber(5)),
+          product_name: faker.commerce.productName(),
+          desc: faker.commerce.productDescription(),
+          price: parseInt(faker.finance.amount({ autoFormat: true, min: 5, max: 10 })),
           createdAt: new Date()
         }
       })
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError && error.code === "P2003") {
-        throw new Error("Foreign key constraint failed on the field or not found")
+        throw new DatabaseErrorConstraint(
+          error.name,
+          "Error Database",
+          "Foreign key constraint failed on the field or not found"
+        )
       }
       throw error
     }
   }
 
-  public async storeUser(payload: Omit<AuthRegisterType, "id">): Promise<AuthRegisterType | any> {
+  public async storeUser(payload: AuthRegister): Promise<AuthRegisterType> {
     try {
-      return await this.prisma.users.create({ data: payload })
+      return await this.prisma.users.create({ data: { ...payload, user_id: uuidV7() } })
     } catch (error: any) {
       if (error instanceof PrismaClientKnownRequestError && error.code === "P2002") {
-        throw new Error("Email has been registered")
+        throw new DatabaseErrorConstraint(error.name, "Error Database", "Email has been registered")
       }
       throw error
     }
   }
 }
-
-export default new FakerService()
