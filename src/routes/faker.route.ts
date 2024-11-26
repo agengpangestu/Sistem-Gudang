efcb47e331612284c3e7e9032c11ebda097949bd
agengpangestu <kapalmabur002@gmail.com>
2024-11-26 12:02:31 +0700

    update: - fix route faker - added validation error joi for product route

--- src/routes/faker.route.ts
+++ src/routes/faker.route.ts
@@ -1,8 +1,14 @@
 import { Router } from "express"
-import faker from "../utils/faker"
+import { FakerController } from "../utils/faker"
 import { RequiredAdmin } from "../middleware/auth"
+import PrismaUtils from "../utils/prisma"
+import { FakerService } from "../services"
+import { ValidateError } from "../middleware"
+import { ProductValidation } from "../validation"
 
+const prisma = new PrismaUtils()
+const fakerController = new FakerController(new FakerService(prisma))
 export const FakerRoute: Router = Router()
 
-FakerRoute.post("/product", RequiredAdmin, faker.storeProduct)
-FakerRoute.post("/user", RequiredAdmin, faker.storeUser)
+FakerRoute.post("/product", RequiredAdmin, ValidateError(ProductValidation), fakerController.storeProduct)
+FakerRoute.post("/user", RequiredAdmin, fakerController.storeUser)
