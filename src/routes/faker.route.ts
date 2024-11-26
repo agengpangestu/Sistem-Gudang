932bfe8647d54551e76d45248f30b93d8c4664c0
agengpangestu <kapalmabur002@gmail.com>
2024-11-26 12:03:38 +0700

    update: delete validate error joi from product route faker

--- src/routes/faker.route.ts
+++ src/routes/faker.route.ts
@@ -3,12 +3,10 @@ import { FakerController } from "../utils/faker"
 import { RequiredAdmin } from "../middleware/auth"
 import PrismaUtils from "../utils/prisma"
 import { FakerService } from "../services"
-import { ValidateError } from "../middleware"
-import { ProductValidation } from "../validation"
 
 const prisma = new PrismaUtils()
 const fakerController = new FakerController(new FakerService(prisma))
 export const FakerRoute: Router = Router()
 
-FakerRoute.post("/product", RequiredAdmin, ValidateError(ProductValidation), fakerController.storeProduct)
+FakerRoute.post("/product", RequiredAdmin, fakerController.storeProduct)
 FakerRoute.post("/user", RequiredAdmin, fakerController.storeUser)
