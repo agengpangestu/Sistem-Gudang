79b744a73b8ce6831384b0fe5cf3eadd8672edfa
Ageng Pangestu <agengp617@gmail.com>
2024-12-18 09:00:26 +0700

    update: added new property in environment file

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,7 +3,12 @@ import "dotenv/config"
 const CONFIG = {
   port: process.env.APP_PORT as string,
   jwt_public: process.env.JWT_PUBLIC as string,
-  jwt_private: process.env.JWT_PRIVATE as string
+  jwt_private: process.env.JWT_PRIVATE as string,
+  host: process.env.HOST as string,
+  username: process.env.USERNAME as string,
+  password: process.env.PASSWORD as string,
+  database: process.env.DATABASE as string,
+  db_port: process.env.DB_PORT as string
 }
 
 export default CONFIG
