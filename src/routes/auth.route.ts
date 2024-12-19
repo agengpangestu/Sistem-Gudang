106862e84aacd7967d32bacac5c5e037c849543f
Ageng Pangestu <agengp617@gmail.com>
2024-12-19 21:05:09 +0700

    update: auth route and delete not use

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,9 @@
 import { Router } from "express"
-import { AuthController } from "../controller"
-import PrismaUtils from "../utils/prisma"
-import { AuthService } from "../services"
+import { Login, Register } from "../controller"
 import { ValidateError } from "../middleware"
 import { LoginValidation, RegisterValidation } from "../validation"
 
-const prisma = new PrismaUtils()
-const authController = new AuthController(new AuthService(prisma))
 export const AuthRoute: Router = Router()
 
-AuthRoute.post("/registration", ValidateError(RegisterValidation), authController.Register)
-AuthRoute.post("/login", ValidateError(LoginValidation), authController.Login)
+AuthRoute.post("/registration", ValidateError(RegisterValidation), Register)
+AuthRoute.post("/login", ValidateError(LoginValidation), Login)
